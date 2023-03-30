0487fea066762a44e5e2f5139020c7452a5ee678
jungla-team <44529886+jungla-team@users.noreply.github.com>
2023-03-30 18:39:51 +0200

    Update speedy_OEA_autoinstall.sh

--- speedy_OEA_autoinstall.sh
+++ speedy_OEA_autoinstall.sh
@@ -58,6 +58,7 @@ echo "🔎 Buscando si la imagen instalada es compatible con la instalacion..."
 if [[ "$DISTRO" == "openatv" ]]; then
     echo -e "${YELLOW}👍 Se ha detectado que tienes instalado OpenATV, se procede a la ejecucion de la instalacion.${RESET}"
     $TIEMPO_ESPERA
+    wget -q '--no-check-certificate' https://raw.githubusercontent.com/jungla-team/Speedy-OEA-autoinstall/main/images-execute/execute_speedy_openatv.sh; sh execute_speedy_openatv.sh; rm -r execute_speedy_openatv.sh
 else
     echo -e "${RED}⛔️ Lo siento, no se ha detectado ninguna imagen compatible, $TEXTO_SALIDA.${RESET}"
     $TIEMPO_ESPERA
