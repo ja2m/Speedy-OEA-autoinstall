b43a8bae2dfa4b6749238ea1202ce03a3ac0101b
jungla-team <44529886+jungla-team@users.noreply.github.com>
2023-05-13 14:08:47 +0200

    Update speedy_OEA_autoinstall.sh

--- speedy_OEA_autoinstall.sh
+++ speedy_OEA_autoinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Provides: jungle-team
 # Description: Speedy OEA autoinstall, instalacioon complemenetos.
-# Version: 1.3
-# Date: 01/04/2023
+# Version: 1.4
+# Date: 13/05/2023
 
 # Definimos variables de colores
 GREEN="\e[32m"
